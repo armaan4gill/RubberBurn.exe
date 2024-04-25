b0672168b9d61d09d0379b0305819651f3da2fab
PkKev67 <oajohnson8@gmail.com>
2024-04-25 11:53:14 -0700

    Car accelleration

--- RubberBurn/Assets/Personal/Owen/Scripts/ChaserEnemyScript.cs
+++ RubberBurn/Assets/Personal/Owen/Scripts/ChaserEnemyScript.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 
 public class ChaserEnemyScript : MonoBehaviour
 {
+    Rigidbody rb;
     public float speed;
 
     int enemyHealth = 1;
     int enemyDamage = 25;
+
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
 
     void Update()
     {
-
+        rb.AddForce(speed, 0, 0, ForceMode.Acceleration);
+        print("adding force");
     }
 }
