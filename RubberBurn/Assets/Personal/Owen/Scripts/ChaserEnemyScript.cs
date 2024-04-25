32210346da503a91e9efaedf3146055a46fc23b9
PkKev67 <oajohnson8@gmail.com>
2024-04-25 11:05:06 -0700

    Made chaser enemy

---
+++ RubberBurn/Assets/Personal/Owen/Scripts/ChaserEnemyScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaserEnemyScript : MonoBehaviour
+{
+    private float speed;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+}
