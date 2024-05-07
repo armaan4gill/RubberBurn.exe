6ac1242fcf3060a838648473c6722d98950956fc
PkKev67 <oajohnson8@gmail.com>
2024-05-07 11:50:38 -0700

    stun gun added

---
+++ RubberBurn/Assets/Personal/Owen/Scripts/Spawner.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    //vars
+    public bool goingLeft;
+    public float timeBetweenShots;
+    public float startDelay;
+    public GameObject projectilePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnProjectile", startDelay, timeBetweenShots);
+    }
+
+    public void SpawnProjectile()
+    {
+        GameObject projectile = Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        if (projectile.GetComponent<Laser>())
+        {
+            projectile.GetComponent<Laser>().goingLeft = goingLeft;
+        }
+    }
+}
